db34bb64b7244e807bcfe29f0cb82789559e216c
gavo11111 <lavigne_cuau@hotmail.com>
2025-09-06 05:57:23 +0000

    empezado

---
+++ src/js/components/index.js
@@ -0,0 +1,12 @@
+function SimpleCounter(props){
+return (<div className="granContador">
+    <div className="calendario">
+        <i class="fa-solid fa-clock"></i>
+    </div>
+    <div className="cuarto">0</div>
+    <div className="tres">0</div>
+    <div className="dos">0</div>
+    <div className="uno">0</div>
+
+</div>);
+}
