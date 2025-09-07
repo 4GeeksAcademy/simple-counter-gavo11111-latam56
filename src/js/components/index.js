d29c873083232360bb545966f2bcfd61df14cb3a
gavo11111 <lavigne_cuau@hotmail.com>
2025-09-07 05:56:18 +0000

    casi terminado pero con errores

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,12 +1,42 @@
-function SimpleCounter(props){
-return (<div className="granContador">
-    <div className="calendario">
-        <i class="fa-solid fa-clock"></i>
-    </div>
-    <div className="cuarto">0</div>
-    <div className="tres">0</div>
-    <div className="dos">0</div>
-    <div className="uno">0</div>
+import React from 'react';
+import ReactDOM from'react-dom';
+import PropTypes from 'prop-types';
+import 'bootstrap';
+import './icons.js';
+import '../styles/index.scss';
 
+
+
+
+
+function SimpleCounter(props){
+return (<div className="bigCounter">
+    <div className="calendar">
+        <i class="far fa-clock"></i>
+    </div>
+    <div className="four">{props.digitFour % 10}</div>
+    <div className="three">{props.digitThree % 10}</div>
+    <div className="two">{props.digitTwo % 10}</div>
+    <div className="one">{props.digitOne % 10}</div>
 </div>);
 }
+SimpleCounter.propTypes = { 
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+};
+let conunter = 0;
+setInterval(function(){
+const four = Math.floor(counter/1000);
+const three = Math.floor(counter/100);
+const two = Math.floor(counter/10);
+const one = Math.floor(counter/1);
+console.log(four, three, two, one);
+
+counter ++;
+}, 1000);
+ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four } />,
+    document.querySelector('#app')
+);
